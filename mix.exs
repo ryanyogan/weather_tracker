849bcf2fa6e5cb9f81fa24151642a4341dcdbce6
ryan <ryanyogan@gmail.com>
2021-09-27 22:25:34 -0500

    WIP - Working on inbound weather posts

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule WeatherTracker.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:decimal, "~> 2.0.0"}
     ]
   end
 
